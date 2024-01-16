dee24c8d6938fa80fe82499b039260c3aa905ea2
Golikov-Andrey <newsc2@yandex.ru>
2024-01-16 23:05:24 +0300

    Repeat

--- Seminar3/sem3HomeTask/src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
+++ Seminar3/sem3HomeTask/src/main/java/com/example/sem3HomeTask/services/RegistrationService.java
@@ -1,2 +1,20 @@
-package com.example.sem3HomeTask.services;public class RegistrationService {
+package com.example.sem3HomeTask.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationService {
+
+
+    public DataProcessingService getDataProcessingService() {
+        return dataProcessingService;
+    }
+
+    @Autowired
+    private DataProcessingService dataProcessingService;
+
+    //Поля UserService, NotificationService
+
+    //Метод processRegistration
 }
